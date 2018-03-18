a38962302adcf3a7fcb14ee4cdd88550ac91059f
buyibuyi001 <361143840@qq.com>
2018-03-18 12:24:32 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,56 +1,53 @@
 #include<stdio.h>
-int path=1,maxpath=1;
+/*
+5 5
+1  2  3  4  5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
+*/
 int row=0,col=0;
-int height[110][110]={0},length[110][110]={0};
-int get_ij_length(int,int);
+int next[4][2]={{0,1},{1,0},{-1,0},{0,-1}};
+int hght[110][110]={0},len[110][110]={0};
 
-int ijhigher(int i,int j,int x,int y)
+int get_len(int i,int j)
 {
-    if ( x>=0 && x<row && y>=0 && y<col && height[i][j]>height[x][y] )
+    //len[i][j]+=1;
+    if (len[i][j]!=0)
+        return len[i][j];
+    int curpath=0;int curmaxpath=0;
+    for (int k=0;k<4;k++)
+    {
+        int ni=i+next[k][0];
+        int nj=j+next[k][1];
+        if ( ni>=0 && ni<row && nj>=0 && nj<col && hght[i][j]>hght[ni][nj] )
         {
-            //printf("high");
-            if(length[x][y]==0)
-                get_ij_length(x,y);
-            path=1+length[x][y];
-            maxpath=path>maxpath?path:maxpath;
-            //printf("path%-3d lengthij %d \n",path,length[i][j]);
+            curpath=1+get_len(ni,nj);
+            curmaxpath= curmaxpath>curpath? curmaxpath:curpath;
         }
-    return 0;
-}
-
-int get_ij_length(int i,int j)
-{
-    //printf("h%d%d%-3d ",i,j,height[i][j]);
-    ijhigher(i,j,i-1,j);
-    ijhigher(i,j,i,j-1);
-    ijhigher(i,j,i+1,j);
-    ijhigher(i,j,i,j+1);
-    length[i][j]=maxpath;
-    path=1;
-    maxpath=1
-
-    return 0;
+    }
+    return curmaxpath;
 }
 
 int main (void)
 {
     int i=0,j=0;
-    int max_ij_length=1;
+    int max_len=0;int curmax=0;
     scanf("%d %d",&row,&col);
-
     for (i=0;i<row;i++)
         for (j=0;j<col;j++)
-            scanf("%d",&height[i][j]);
-
-    length[0][0]=1;
+            scanf("%d",&hght[i][j]);
     for (i=0;i<row;i++)
         for (j=0;j<col;j++)
         {
-            get_ij_length(i,j);
-            max_ij_length= max_ij_length>length[i][j] ? max_ij_length:length[i][j];
+            len[i][j]=get_len(i,j);
+            max_len= max_len>len[i][j] ? max_len:len[i][j];
         }
-    printf("%d ", max_ij_length);
+    printf("%d \n", max_len);
+    for (i=0;i<row;i++){
+        for (j=0;j<col;j++)
+            printf("%-3d ",len[i][j]);
+        printf("\n"); }
     return 0;
 }
-
-
